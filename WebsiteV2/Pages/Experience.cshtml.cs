bcda6ff88b048517e7c8d45d3cbaab0a0d06c444
Brasen Xu <brasenxu@gmail.com>
2024-10-09 22:30:21 -0400

    first website commit

---
+++ WebsiteV2/Pages/Experience.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebsiteV2.Pages;
+
+public class ExperienceModel(ILogger<ExperienceModel> logger) : PageModel
+{
+    private readonly ILogger<ExperienceModel> _logger = logger;
+
+    public void OnGet()
+    {
+    }
+}
---
+++ WebsiteV2/Pages/Projects.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebsiteV2.Pages;
+
+public class ProjectsModel(ILogger<ProjectsModel> logger) : PageModel
+{
+    private readonly ILogger<ProjectsModel> _logger = logger;
+
+    public void OnGet()
+    {
+        
+    }
+}
