2b8c2644503a81f1584bfaeed67a3dfd34edebc8
Brasen Xu <brasenxu@gmail.com>
2025-10-09 00:12:18 -0400

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/next"
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
 const geistSans = Geist({
@@ -42,6 +43,7 @@ export default function RootLayout({
             {children}
           </main>
         </div>
+        <Analytics />
       </body>
     </html>
   );
